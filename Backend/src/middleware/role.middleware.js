1672162c068432174c39e7065956b7802d12e7db
nirob <nirobprottoy12@gmail.com>
2026-04-27 01:38:29 +0600

    New Middleware implemented and userAuth little error solved

---
+++ Backend/src/middleware/role.middleware.js
@@ -0,0 +1,25 @@
+import { User } from "../models/User.models"
+import {ApiError} from '../utils/apiError.js'
+
+// this middleware will be used in routes after the authUser....
+//like this--> verifyjwt, restrictTo['doctor],controller
+
+export const restrictTo = (allowedRoles) =>{
+   return (req,res,next)=>{
+
+    if (!allowedRoles.includes(req.user?.role)) {
+        throw new ApiError(
+          403,
+          `Forbidden: Users with role '${req.user?.role}' do not have permission to access this route.`
+        );
+    }
+
+    if (req.user?.status !== "ACTIVE") {
+      throw new ApiError(
+        403,
+        `Access Denied: Your account status is currently ${req.user?.status}. Contact Administration.`
+      );
+    }
+    next ();
+   }
+}
--- Backend/src/middleware/userAuth.middleware.js
+++ Backend/src/middleware/userAuth.middleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user.model.js';
 import {asyncHandler} from '../utils/asyncHandler.js';
+import { ApiError } from "../utils/apiError.js";
 
 export const verifyJWT =asyncHandler(async(req,res,next)=>{
 
@@ -25,5 +26,4 @@ next()
 } catch (error) {
     throw new ApiError(401,error?.message || "invalid access token")
 }
-
 }) 
