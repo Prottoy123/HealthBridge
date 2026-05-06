8c86bf061757e47561a1b8f76f58506fd3a23cb5
nirob <nirobprottoy12@gmail.com>
2026-05-06 19:20:30 +0600

    Fix import path for User model in userAuth middleware

--- Backend/src/middleware/userAuth.middleware.js
+++ Backend/src/middleware/userAuth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { User } from '../models/user.model.js';
+import { User } from '../models/User.models.js';
 import {asyncHandler} from '../utils/asyncHandler.js';
 import { ApiError } from "../utils/apiError.js";
 
