70e5e68a811e124c4bdd70b281cb7dcac5156098
nirob <nirobprottoy12@gmail.com>
2026-05-08 19:44:42 +0600

    Scoket Server Intialize

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,25 +1,55 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { createServer } from "http";
+import { Server } from "socket.io"; 
 
 const app = express();
+const server = createServer(app);
+
+// 4. Socket.io ইনিশিয়ালাইজ করা এবং CORS সেট করা (খুবই জরুরি)
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  },
+});
+
+// 5. দ্য ইনভিজিবল ব্রিজ: গ্লোবাল ভেরিয়েবল হিসেবে io-কে Express-এ সেভ করা
+app.set("io", io);
 
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
-    Credentials: true,
+    credentials: true, 
   })
 );
 
-app.use(express.json({ limit: "16kb" })); // this is used to parse the json data from the request body and limit the size of the json data to 16kb. this is done to prevent the denial of service attack by sending large json data in the request body.
-app.use(express.urlencoded({ extended: true, limit: "16kb" })); // this is used to parse the urlencoded data from the request body and limit the size of the urlencoded data to 16kb. this is done to prevent the denial of service attack by sending large urlencoded data in the request body.
-app.use(express.static("public")); // this is used to serve the static files from the public folder. this is done to serve the uploaded files like avatar and cover image from the public folder. when we upload the files using multer middleware, it will save the files in the public folder and we can access those files using the url like http://localhost:5000/avatar/filename or http://localhost:5000/coverImage/filename.
-app.use(cookieParser()); // this is used to parse the cookies from the request header and make it available in the req.cookies object. this is done to get the refresh token from the cookies when we want to refresh the access token using the refresh token. we can get the refresh token from the req.cookies.refreshToken and use it to generate a new access token.
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//Socket Connection
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  //join the room
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    console.log(`User ${socket.id} silently joined room: ${roomName}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
 
 //Routes
 import UserRouter from "./routes/user.routes.js";
 
 //routes declaration
-app.use("/api/v1/user",UserRouter );
+app.use("/api/v1/user", UserRouter);
 
-export { app };
+// 7. Export the explicitly created 'server' along with 'app'
+export { app, server, io };
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
-import { app } from "./app.js";
+import { server } from "./app.js";
 
 dotenv.config({
   path: "./.env",
@@ -8,11 +8,10 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`server is running at port: 
-            ${process.env.PORT}`);
+    server.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
     });
   })
   .catch((err) => {
-    console.log("MOngo db connection failed !!!", err);
+    console.log("MongoDB connection failed !!!", err);
   });
