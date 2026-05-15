12c01f2a1b3569613db78954e08747312c819bb3
nirob <nirobprottoy12@gmail.com>
2026-05-15 22:33:21 +0600

    doctor route implemented

---
+++ Backend/src/routes/doctor.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { verifyJWT } from "../middleware/userAuth.middleware.js";
+import { restrictTo } from "../middleware/role.middleware.js";
+import { completeVisit, getDailySchedule, updateDoctorProfile } from "../controller/doctor.controller.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+// Doctor specific routes - Restricted to "DOCTOR" role only
+router.route("/daily-schedule").get(restrictTo("DOCTOR"), getDailySchedule);
+
+router
+  .route("/complete-visit/:appointmentId")
+  .patch(restrictTo("DOCTOR"), completeVisit);
+
+router.route("/profile").patch(restrictTo("DOCTOR"), updateDoctorProfile);
+
+export default router;
