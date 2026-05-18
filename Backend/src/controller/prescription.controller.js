3c229a85c247c53ea88d675a3f4eb3aee89fb08d
nirob <nirobprottoy12@gmail.com>
2026-05-18 17:31:12 +0600

    Prescription controller completed

--- Backend/src/controller/prescription.controller.js
+++ Backend/src/controller/prescription.controller.js
@@ -5,7 +5,7 @@ import { asyncHandler } from "../utils/AsyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { Prescription } from "../models/prescription.models.js";
 
-const uploadPrescription = asyncHandler(async (req, res) => {
+export const uploadPrescription = asyncHandler(async (req, res) => {
   const { title, doctorName, prescriptionDate } = req.body;
 
   if (!title || !doctorName || !prescriptionDate) {
@@ -47,7 +47,7 @@ const uploadPrescription = asyncHandler(async (req, res) => {
     );
 });
 
-const getMyPrescriptions = asyncHandler(async(req,res)=>{
+export const getMyPrescriptions = asyncHandler(async(req,res)=>{
 
   const {page=1,limit=10} = req.query;
 
@@ -68,8 +68,8 @@ const getMyPrescriptions = asyncHandler(async(req,res)=>{
       },
     ];
 
-    const aggregateQuery = await Prescription.aggregate(pipeline);
-    const prescriptions = await Prescription.aggregatePaginate(pipeline, {
+    const aggregateQuery = Prescription.aggregate(pipeline);
+    const prescriptions = await Prescription.aggregatePaginate(aggregateQuery, {
       page: pageNumber,
       limit: limitNumber,
     });
@@ -96,8 +96,66 @@ const getMyPrescriptions = asyncHandler(async(req,res)=>{
       );
 
 })
+export const getPatientPrescriptions = asyncHandler(async(req,res)=>{
+ const {patientId} = req.params;
+ const {page=1,limit=10} = req.query;
 
-const getPatientPrescriptions = asyncHandler(async(req,res)=>{
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
 
+    const thresholdDate = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+
+  const verifyUser = await Appointment.findOne({
+    patientId: patientId,
+    doctorId: req.user._id,
+    status: {
+      $in: ["COMPLETED", "CONFIRMED"],
+    },
+    appointmentDate: { $gte: thresholdDate },
+  });
+
+  if (!verifyUser) {
+    throw new ApiError(403,"You dont have authority to view this")
+  }
+
+  const pipeline = [
+    {
+      $match: {
+        patientId: new mongoose.Types.ObjectId(patientId),
+      },
+    },
+    {
+      $sort: {
+        prescriptionDate: -1,
+      },
+    },
+  ];
+
+      const aggregateQuery = Prescription.aggregate(pipeline);
+      const prescriptions = await Prescription.aggregatePaginate(aggregateQuery, {
+        page: pageNumber,
+        limit: limitNumber,
+      });
+
+      if (!prescriptions) {
+        throw new ApiError(500, "Failed to fetch prescriptions");
+      }
+
+      const payload = {
+        records: prescriptions.docs,
+        pagination: {
+          totalDocs: prescriptions.totalDocs,
+          totalPages: prescriptions.totalPages,
+          currentPage: prescriptions.page,
+          hasNextPage: prescriptions.hasNextPage,
+          hasPrevPage: prescriptions.hasPrevPage,
+        },
+      };
+
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(200, payload, "Prescriptions fetched successfully")
+        ); 
   
 })
