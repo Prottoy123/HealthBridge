822fa2eddce98d9dbe7afdd6dd9df67e8daf2211
nirob <nirobprottoy12@gmail.com>
2026-05-18 17:31:24 +0600

    prescription routes completed

---
+++ Backend/src/routes/prescription.routes.js
@@ -0,0 +1,35 @@
+import { Router } from "express";
+
+import { verifyJWT } from "../middleware/userAuth.middleware.js";
+import { restrictTo } from "../middleware/role.middleware.js";
+import { upload } from "../middleware/multer.middleware.js";
+import {
+  getMyPrescriptions,
+  getPatientPrescriptions,
+  uploadPrescription,
+} from "../controller/prescription.controller.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+// POST /api/v1/prescriptions/upload
+router
+  .route("/upload")
+  .post(
+    restrictTo("PATIENT"),
+    upload.fields([{ name: "prescriptionImage", maxCount: 1 }]),
+    uploadPrescription
+  );
+
+// GET /api/v1/prescriptions/my-prescriptions
+router
+  .route("/my-prescriptions")
+  .get(restrictTo("PATIENT"), getMyPrescriptions);
+
+// GET /api/v1/prescriptions/patient/:patientId
+router
+  .route("/patient/:patientId")
+  .get(restrictTo("DOCTOR"), getPatientPrescriptions);
+
+export default router;
