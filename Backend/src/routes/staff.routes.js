918202ea87e9ecd6f0dd3ee1b2713af1e1307ffc
nirob <nirobprottoy12@gmail.com>
2026-05-26 00:50:25 +0600

    staff router implemeneted

---
+++ Backend/src/routes/staff.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { upload } from "../middleware/multer.middleware.js";
+import { verifyJWT } from "../middleware/userAuth.middleware.js";
+import { restrictTo } from "../middleware/role.middleware.js";
+import { generateSlots, getDoctorAppointments, updateQueueStatus, uploadPatientReport } from "../controller/staff.controller.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+router.use(restrictTo("STAFF"));
+
+router.route("/generate-slots").post(generateSlots);
+
+router.route("/doctor-queue").get(getDoctorAppointments);
+
+router.route("/queue-status/:appointmentId").patch(updateQueueStatus);
+
+router
+  .route("/upload-lab-report")
+  .post(upload.array("recordFiles", 5), uploadPatientReport);
+
+export default router;
