8860bf2d6f0cc723913b3d25f7b48473d8abe74c
nirob <nirobprottoy12@gmail.com>
2026-06-22 03:24:30 +0600

    rate kimit middleware implemented using redis

---
+++ Backend/src/middleware/rateLimiter.middleware.js
@@ -0,0 +1,26 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { getRedis } from "../config/redis.config.js";
+
+export const rateLimiter = (keyPrefix, limit, windowTime) => {
+  return asyncHandler(async (req, res, next) => {
+    const redisClient = getRedis();
+
+    const key = `${keyPrefix}:${req.user._id}`;
+
+    const requestCount = await redisClient.incr(key);
+
+    if (requestCount === 1) {
+      await redisClient.expire(key, windowTime);
+    }
+
+    if (requestCount > limit) {
+      throw new ApiError(
+        429,
+        `Rate limit exceeded for ${keyPrefix}. Please try again later.`
+      );
+    }
+
+    next();
+  });
+};
