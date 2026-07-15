828e5c66c9a7e80ae80a1803da98b904f1dabc6d
nirob <nirobprottoy12@gmail.com>
2026-07-15 20:01:26 +0600

    patient id required changed

--- Backend/src/models/appoinment.models.js
+++ Backend/src/models/appoinment.models.js
@@ -4,7 +4,7 @@ const appointmentSchema = new Schema(
     patientId: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      required: false,
     }, 
     doctorId: {
       type: Schema.Types.ObjectId,
